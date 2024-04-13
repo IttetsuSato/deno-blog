e5e07156452a0637419cb2e8171898eeab0b0a38
Ittetsu Sato <ittetsusato@gmail.com>
2024-04-13 10:18:36 +0900

    fix link

--- main.tsx
+++ main.tsx
@@ -1,29 +1,38 @@
 /** @jsx h */
 
 import blog, { ga, redirects, h } from "blog";
-import { Post } from "https://deno.land/x/blog@0.7.0/types.js";
+import { Post } from "https://deno.land/x/blog@0.7.0/types.d.ts";
 
 blog({
   title: "It's Trail",
   description: "This is my new blog.",
-  header: <header>Your custom header</header>,
-  section: (post: Post) => <section>Your custom section with access to Post props.
-  {Post}</section>,
-  footer: <footer>Your custom footer</footer>,
+  // header: <header>Your custom header</header>,
+  section: (post: Post) => (
+    <section>
+      Your custom section with access to Post props.
+      {post}
+    </section>
+  ),
+  links: [
+    { title: "GitHub", url: "https://github.com/IttetsuSato" },
+    { title: "X", url: "https://twitter.com/Ittetsu0501" },
+    { title: "note", url: "https://note.com/ittetsu51" },
+  ],
+  // footer: <footer>Your custom footer</footer>,
   avatar: "./icon.gif",
   avatarClass: "rounded-full",
   author: "Ittetsu Sato",
 
   // middlewares: [
 
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
+  // If you want to set up Google Analytics, paste your GA key here.
+  // ga("UA-XXXXXXXX-X"),
 
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
+  // If you want to provide some redirections, you can specify them here,
+  // pathname specified in a key will redirect to pathname in the value.
+  // redirects({
+  //  "/hello_world.html": "/hello_world",
+  // }),
 
   // ]
 });
